28eddd1a0424ddfe220c66071a4f9ac0afd1eb1c
Lady Stock <nikki.castlea@gmail.com>
2020-06-28 15:01:14 -0600

    psuedocode started

---
+++ script.js
@@ -0,0 +1,11 @@
+// Should the questions be inside alerts?
+// There should be a start button, which then starts the timer (timer set for certain length of time, maybe 60 seconds?)
+
+// Once user clicks start, start button should disappear and first question should appear. 
+
+// When first question if answered:
+    // If it's correct, goes to next question. If it's wrong, time is subtracted from the timer. 
+
+    // When timer get to zero, or when all questions have been answered, which ever comes first
+        // timer stops counting down and either displays 00:00 or disappears
+        // card will appear showing the user's score and time used
