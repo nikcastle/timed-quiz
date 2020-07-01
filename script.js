087ed8bea53c4980a3416075347d9baad5ced4f2
Lady Stock <nikki.castlea@gmail.com>
2020-07-01 00:11:46 -0600

    working questions

--- script.js
+++ script.js
@@ -7,39 +7,26 @@
 // Assignment code
 var secondsLeft = 60
 var startQuiz = document.querySelector("#start");
-console.log("start quiz", startQuiz);
 var questions = [
     {question: "What does HTML stand for?",
     answers: ["Hypertext Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markdown Language", "Hardy Tools Markup Language"],
     correct: "Hypertext Markup Language",},
 
     {question: "In JavaScript, what element is used to store and manipulate text?",
-    answerA: "Recorders",
-    answerB: "Variables",
-    answerC: "Arrays",
-    answerD: "Strings",
+    answers: ["Recorders", "Variables", "Arrays", "Strings"], 
     correct: "Strings",},
 
     {question: "CSS stands for _______ Style Sheets",
-    answerA: "Cascading",
-    answerB: "Combined",
-    answerC: "Concept",
-    answerD: "Charlie's",
+    answers: ["Cascading", "Combined", "Concept", "Charlie's"],
     correct: "Cascading",},
 
     {question: "In JavaScript, what is a block of code called that is used to perform a specfic task?",
-    answerA: "Conditional Statement",
-    answerB: "Variable",
-    answerC: "Declaration",
-    answerD: "Function",
+    answers: ["Conditional Statement", "Variable", "Declaration", "Function"],
     correct: "Function",},
 
     {question: "Which tag is used to define a container for an external app or plug-in in HTML?",
-    answerA: "<code>",
-    answerB: "<embed>",
-    answerC: "<!DOCTYPE>",
-    answerD: "<caption>",
-    correct: "<embed>",},
+    answers: ["<code>", "<embed>", "<!DOCTYPE>", "<caption>"],
+    correct: "<embed>",}
 ]; 
 var score = 0; //score should be stored in local storage. High scores will need ot be retrieved from local storage (don't forget JSON.stringify and JSON.parse). // * Score is amount of time left, not 1 point for each question answered correctly
 var j = 0;
@@ -69,21 +56,27 @@ function checkAnswer (event) {
     if (questions[j].correct !== event.target.textContent) {
         console.log("correct");
         secondsLeft -=5;
+    } else {
+        alert("correct!")
     }
-    if (j < questions.length-1) {j++;
-        renderQuestion();
+    if (j < questions.length) {j++;
+        renderQuestion()
     }
     else endGame();
+}
 
 function endGame () {
-
+    clearInterval;
+    document.querySelector(".results").setAttribute("style", "display:block");
+    storeScores();
+    // localStorage.getItem();
 }
 
 // for timer, use setInterval. When timer = 0 , use clearInterval
 function counter () {
     interval = setInterval(function() {
+        document.querySelector("#timer")
         secondsLeft--;
-
         if(secondsLeft === 0) {
             clearInterval(interval);
             // When timer get to zero, or when all questions have been answered, which ever comes first
@@ -91,12 +84,15 @@ function counter () {
                 // card will appear showing the user's score and time used
 
         }
+        else {
+            return secondsLeft
+        }
     }, 1000);
 }
 
 //commit scores to local storage so they can be pulled later
 function storeScores () {
-    localStorage.setItem()
+    localStorage.setItem("score", secondsLeft)
 }
 
 //call scores from local storage to retrieve high scores
